5ba20c4f91b2b1ef1b3ac72b25e66e614aa4784f
songxiaofeng <1468032125@qq.com>
2013-12-02 23:33:56 +0800

    storyDemo，用于练习动画的.

---
+++ C#/WPF/StoryDemo/StoryDemo/Modules/Storyboard/StoryViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace StoryDemo.Modules.Storyboard
+{
+    class StoryViewModel : Screen
+    {
+        public StoryViewModel()
+        {
+            DisplayName="StoryDemo";
+        }
+    }
+}
---
+++ C#/WPF/StoryDemo/StoryDemo/ShellViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using StoryDemo.Modules.Storyboard;
+
+namespace StoryDemo
+{
+   public class ShellViewModel : Conductor<Screen>.Collection.OneActive
+    {
+       public ShellViewModel()
+       {
+           Items.Add(new StoryViewModel());
+           
+           //Items.Add()
+       }
+
+       
+    }
+}
