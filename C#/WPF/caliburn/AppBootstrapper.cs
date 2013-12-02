b571f176405dfbd78f7780f32359eaff26d02ccc
songxiaofeng <1468032125@qq.com>
2013-12-02 21:51:35 +0800

    创建初始版本;添加了caliburn.mirco的bootstrapper使用Unity作为ioc容器的示例

---
+++ C#/WPF/caliburn/AppBootstrapper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using Microsoft.Practices.Unity;
+namespace DoubanDemo
+{
+    public class AppBootstrapper : Bootstrapper<ShellViewModel>
+    {
+        private IUnityContainer container;
+
+        protected override void Configure()
+        {
+            container = new UnityContainer();
+            container.RegisterType<IWindowManager, WindowManager>(new ContainerControlledLifetimeManager());
+            container.RegisterType<IEventAggregator, EventAggregator>(new ContainerControlledLifetimeManager());
+            container.RegisterInstance(container);
+        }
+
+        protected override object GetInstance(Type serviceType, string key)
+        {
+           return  container.Resolve(serviceType, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type serviceType)
+        {
+
+            return container.ResolveAll(serviceType);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            container.BuildUp(instance);
+        }
+      
+    }
+}
