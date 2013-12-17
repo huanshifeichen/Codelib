6bec2d5a6f51fbb5c2c67218a6a2c02d8f88f58c
songxiaofeng <1468032125@qq.com>
2013-12-17 23:23:43 +0800

    wp的caliburn app文件

---
+++ C#/WP/caliburn/App.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+using System.Resources;
+using System.Windows;
+using System.Windows.Markup;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Demo01.Resources;
+
+namespace Demo01
+{
+    public partial class App : Application
+    {
+        /// <summary>
+        ///提供对电话应用程序的根框架的轻松访问。
+        /// </summary>
+        /// <returns>电话应用程序的根框架。</returns>
+        public static PhoneApplicationFrame RootFrame { get; private set; }
+
+        /// <summary>
+        /// Application 对象的构造函数。
+        /// </summary>
+        public App()
+        {
+            InitializeComponent();
+            // Show graphics profiling information while debugging.
+            if (Debugger.IsAttached)
+            {
+                // Display the current frame rate counters.
+                Application.Current.Host.Settings.EnableFrameRateCounter = true;
+
+                // Show the areas of the app that are being redrawn in each frame.
+                //Application.Current.Host.Settings.EnableRedrawRegions = true;
+
+                // Enable non-production analysis visualization mode,
+                // which shows areas of a page that are handed off to GPU with a colored overlay.
+                //Application.Current.Host.Settings.EnableCacheVisualization = true;
+
+                // Prevent the screen from turning off while under the debugger by disabling
+                // the application's idle detection.
+                // Caution:- Use this under debug mode only. Application that disables user idle detection will continue to run
+                // and consume battery power when the user is not using the phone.
+                PhoneApplicationService.Current.UserIdleDetectionMode = IdleDetectionMode.Disabled;
+            }
+        }
+    }
+}
