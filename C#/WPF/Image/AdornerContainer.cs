57ff3a76f08745f14d04c10c006ec93cc95c9bcf
huanshifeichen <partykillers@gmail.com>
2014-01-07 18:13:52 +0800

    用Adorner实现截图效果
    AdornerContainer可以让任意UIElement处于Adorner层.DrawingCanvas一个可以绘制截图效果(没实际截图)的用户控件.

---
+++ C#/WPF/Image/AdornerContainer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+using System.Windows;
+using System.Windows.Controls;
+namespace ImageDemo
+{
+  public  class AdornerContainer : Adorner
+    {
+      public AdornerContainer(UIElement adornerElement): base(adornerElement)
+      {
+
+      }
+
+      private UIElement child;
+
+      public UIElement Child
+      {
+          get { return child; }
+          set {
+              if (child != value)
+              {
+                  RemoveLogicalChild(child);
+                  RemoveVisualChild(child);
+                  child = value;
+                  AddLogicalChild(child);
+                  AddVisualChild(child);
+              }
+          }
+      }
+
+
+      protected override int VisualChildrenCount
+      {
+          get
+          {
+              return this.child == null ? 0 : 1;
+          }
+      }
+
+      protected override System.Windows.Media.Visual GetVisualChild(int index)
+      {
+          return this.child;
+      }
+
+
+
+      protected override Size MeasureOverride(Size constraint)
+      {
+          if (child !=null)
+          {
+              child.Measure(constraint);
+          }
+          return AdornedElement.RenderSize;
+      }
+
+      protected override Size ArrangeOverride(Size finalSize)
+      {
+          child.Arrange(new Rect(finalSize));
+          return child.RenderSize;
+      }
+
+    }
+}
---
+++ C#/WPF/Image/DrawingCanvas.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace ImageDemo
+{
+    /// <summary>
+    /// DrawingCanvas.xaml 的交互逻辑
+    /// </summary>
+    public partial class DrawingCanvas : UserControl
+    {
+        public DrawingCanvas()
+        {
+            InitializeComponent();
+            rect = new Rectangle();
+           // rect.Fill = Brushes.Blue;
+            //rect.Stroke = Brushes.Black;
+            //rect.StrokeDashArray = new DoubleCollection() { 1,2 };
+        }
+        private Point originPoint;
+        private Point endPoint;
+        public Rectangle rect;
+        private void LeftButtonDownOnClick(object sender, MouseButtonEventArgs e)
+        {
+            originPoint = e.GetPosition(canvas);
+            canvas.Children.Clear();
+           
+        }
+
+        private void MouseOnMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                endPoint = e.GetPosition(canvas);
+                Rect targetRect = new Rect(originPoint, endPoint);
+                rect.Width = targetRect.Width;
+                rect.Height = targetRect.Height;
+                
+                //获取装饰元素作为VisualBrush的来源
+                VisualBrush vb = new VisualBrush(DecoratedElement);
+                vb.Viewbox = targetRect;
+                vb.ViewboxUnits = BrushMappingMode.Absolute;
+
+                rect.Fill = vb;
+
+                Canvas.SetTop(rect, targetRect.Top);
+                Canvas.SetLeft(rect, targetRect.Left);
+                if (canvas.Children.Count==0)
+                {
+                    canvas.Children.Add(rect);
+                }
+
+
+               Debug.WriteLine(string.Format("endPoint x:{0},y:{1}", endPoint.X, endPoint.Y));
+            }
+        }
+
+        public UIElement DecoratedElement { get; set; }
+
+    }
+}
